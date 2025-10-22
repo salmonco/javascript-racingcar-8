a211b229586c12b8b12687a74c26a280d2490817
salmonco <xkqjsslsek80@naver.com>
2025-10-22 19:20:58 +0900

    refactor: add io func

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
 import { InputParser } from "./InputParser.js";
 import { Race } from "./Race.js";
+import { readRacingCarsInput, readTryCountInput } from "./io.js";
 
 class App {
   async run() {
-    const racingCarsInput = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const tryCountInput =
-      await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const racingCarsInput = await readRacingCarsInput();
+    const tryCountInput = await readTryCountInput();
 
     const inputParser = new InputParser(racingCarsInput, tryCountInput);
     const racingCars = inputParser.getRacingCars();
