8260f61f5f6ccaecebb214e9c505800210831c23
salmonco <xkqjsslsek80@naver.com>
2025-10-26 16:44:54 +0900

    test(Race): add public method test

---
+++ __tests__/RaceTest.js
@@ -0,0 +1,73 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { Race } from "../src/controllers/Race.js";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange
+  );
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("RaceTest", () => {
+  test("start", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni,tabby", "1"];
+    const logs = ["pobi : -", "woni : ", "tabby : ", "최종 우승자 : pobi"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([MOVING_FORWARD, STOP, STOP]);
+
+    // when
+    const race = new Race();
+    await race.start();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+
+  test("예외 테스트 - 자동차 이름은 5자 이하만 가능", async () => {
+    // given
+    const inputs = ["pobi,javaji"];
+    mockQuestions(inputs);
+
+    // when
+    const race = new Race();
+
+    // then
+    await expect(race.start()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트 - 시도 횟수는 양의 정수여야 함", async () => {
+    // given
+    const inputs = ["pobi,woni", "-1"];
+    mockQuestions(inputs);
+
+    // when
+    const race = new Race();
+
+    // then
+    await expect(race.start()).rejects.toThrow("[ERROR]");
+  });
+});
