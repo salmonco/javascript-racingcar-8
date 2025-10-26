c1773e255a2f5302ddf5250abf32f7400651fa9d
salmonco <xkqjsslsek80@naver.com>
2025-10-26 17:22:20 +0900

    test(Car): add checkIsMovingForward test

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,4 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import { Car } from "../src/models/Car.js";
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange
+  );
+};
 
 describe("CarTest", () => {
   let car;
@@ -8,14 +18,40 @@ describe("CarTest", () => {
   });
 
   test("moveForward - 1회 전진", () => {
+    // when
     car.moveForward();
+
+    // then
     expect(car.movingForwardCount).toBe(1);
   });
 
   test("moveForward - 10회 전진", () => {
+    // when
     for (let i = 0; i < 10; i++) {
       car.moveForward();
     }
+
+    // then
     expect(car.movingForwardCount).toBe(10);
+  });
+
+  test("checkIsMovingForward - 전진", () => {
+    // given
+    const MOVING_FORWARD = 4;
+
+    mockRandoms([MOVING_FORWARD]);
+
+    // when, then
+    expect(car.checkIsMovingForward()).toBe(true);
+  });
+
+  test("checkIsMovingForward - 정지", () => {
+    // given
+    const STOP = 3;
+
+    mockRandoms([STOP]);
+
+    // when, then
+    expect(car.checkIsMovingForward()).toBe(false);
   });
 });
